b98bcbcea946f45cc2a72b5a7ca48106fb36222c
joshstolz <josh.stolz@libd.org>
2021-11-23 11:30:12 -0500

    update descriptions in getDegTx.R

--- R/getDegTx.R
+++ R/getDegTx.R
@@ -1,8 +1,16 @@
-#' Title
+#' Funtion to obtain expression matrix for degraded transcripts
 #'
-#' @param rse_tx
+#' This function is used to obtain a [RangedSummarizedExperiment-class][SummarizedExperiment::RangedSummarizedExperiment-class] of transcripts and their expression values #' These transcripts are selected based on a prior study of RNA degradation in postmortem brain tissues. This object can later be used to obtain the principle components
+#' necessary to remove the effect of degradation in differential expression.
+#' @param rse_tx A [RangedSummarizedExperiment-class][SummarizedExperiment::RangedSummarizedExperiment-class] object containing
+#' the transcript data desired to be studied.
 #'
-#' @return
+#' @param sig_transcripts A list of transcripts determined to haave degradation signal in the qsva expanded paper.
+#'
+#' @return A
+#'  [RangedSummarizedExperiment-class][SummarizedExperiment::RangedSummarizedExperiment-class]
+#'  object.
+#'
 #' @export
 #'
 #' @examples
