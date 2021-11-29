41e13ba356d0e6425747c9bd36c86c1bcfd4d7d8
joshstolz <josh.stolz@libd.org>
2021-11-29 16:16:54 -0500

    update documentation on parameters and return

--- R/k_qsvs.R
+++ R/k_qsvs.R
@@ -1,11 +1,13 @@
-#' TODO Title
+
+
+#' Apply num.sv algorithm to determine the number of pcs to be included
 #'
-#' TODO Description.s
 #'
-#' @param covComb_tx TODO
-#' @param mod_tx TODO
 #'
-#' @return
+#' @param covComb_tx Ranged Summarizeed Experiment with only trancsripts selected for qsva
+#' @param mod_tx  Model Matrix with necessary variables the you would model for in differential expression
+#'
+#' @return integer representing number of pcs to be included
 #' @export
 #' @importFrom sva num.sv
 #' @importFrom SummarizedExperiment assays
