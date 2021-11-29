0427a0d802a6bde23685dc6536d2ae74b828bd0f
joshstolz <josh.stolz@libd.org>
2021-11-29 16:39:42 -0500

    update examples. Will need help with this

--- R/k_qsvs.R
+++ R/k_qsvs.R
@@ -12,7 +12,7 @@
 #' @importFrom sva num.sv
 #' @importFrom SummarizedExperiment assays
 #' @examples
-#'
+#' ##TODO
 #'
 k_qsvs<- function(covComb_tx, mod_tx){
         k = num.sv(log2(assays(covComb_tx)$tpm), mod_tx)
