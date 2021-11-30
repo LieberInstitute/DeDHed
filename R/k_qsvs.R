2741c3205a779073ad7fff38c09262546e89cf3b
joshstolz <josh.stolz@libd.org>
2021-11-30 11:51:25 -0500

    remove absent colnames from mod_tx example

--- R/k_qsvs.R
+++ R/k_qsvs.R
@@ -12,8 +12,9 @@
 #' @importFrom sva num.sv
 #' @importFrom SummarizedExperiment assays
 #' @examples
-#' mod_tx <- model.matrix(~ Dx + Age + Sex + mitoRate + Region + rRNA_rate + totalAssignedGene + RIN + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5, data = colData(covComb_tx_deg))
+#' mod_tx <- model.matrix(~ mitoRate + Region + rRNA_rate + totalAssignedGene + RIN , data = colData(covComb_tx_deg))
 #' k_qsvs(covComb_tx_deg,mod_tx)
 k_qsvs<- function(covComb_tx, mod_tx){
         k = num.sv(log2(assays(covComb_tx)$tpm+1), mod_tx)
+        return(k)
 }
