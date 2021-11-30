c84b3b7747e1d2577f0996d55cf94465e1d9e77c
joshstolz <josh.stolz@libd.org>
2021-11-30 11:29:20 -0500

    change name of Dataset to covCombtx_deg

--- data-raw/covComb_tx_deg.R
+++ data-raw/covComb_tx_deg.R
@@ -1,4 +1,4 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `covComb_tx_deg` dataset goes here
 load("data/covComb_tx_deg.rda")
 
 usethis::use_data(covComb_tx_deg, overwrite = TRUE)
