94dc73a2f56d430a033c98b84bca0963c4428a18
joshstolz <josh.stolz@libd.org>
2021-12-07 10:54:29 -0500

    add assayname to k_qsvs

--- R/k_qsvs.R
+++ R/k_qsvs.R
@@ -6,6 +6,7 @@
 #'
 #' @param covComb_tx Ranged Summarizeed Experiment with only trancsripts selected for qsva
 #' @param mod_tx  Model Matrix with necessary variables the you would model for in differential expression
+#' @param assayname character string specifying the name of the assay desired in covComb_tx
 #'
 #' @return integer representing number of pcs to be included
 #' @export
@@ -13,8 +14,8 @@
 #' @importFrom SummarizedExperiment assays
 #' @examples
 #' mod_tx <- model.matrix(~ mitoRate + Region + rRNA_rate + totalAssignedGene + RIN , data = colData(covComb_tx_deg))
-#' k_qsvs(covComb_tx_deg,mod_tx)
-k_qsvs<- function(covComb_tx, mod_tx){
-        k = num.sv(log2(assays(covComb_tx)$tpm+1), mod_tx)
+#' k_qsvs(covComb_tx_deg,mod_tx, "tpm")
+k_qsvs<- function(covComb_tx, mod_tx,assayname){
+        k = num.sv(log2(assays(covComb_tx)[[assayname]]+1), mod_tx)
         return(k)
 }
