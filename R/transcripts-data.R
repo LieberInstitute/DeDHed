4cc1ba8a68640b319be77229057aa5a7c2233f9c
joshstolz <josh.stolz@libd.org>
2022-03-22 15:40:44 -0400

    document transcripts data

---
+++ R/transcripts-data.R
@@ -0,0 +1,12 @@
+#' Transcripts for Degradation Models
+#'
+#' TODO.
+#'
+#'
+#'
+#' @name transcripts
+#' @docType data
+#' @format A `list()` with character strings containing the transcripts selected by each model.
+#' Each string is a GENCODE transcript IDs.
+#' @keywords datasets
+#' @seealso [select_transcripts]
