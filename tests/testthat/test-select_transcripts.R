1b28ad36807b108ddff13bd74a85d239c416098f
joshstolz <josh.stolz@libd.org>
2022-03-22 20:52:49 -0400

    remove broken tests

--- tests/testthat/test-select_transcripts.R
+++ tests/testthat/test-select_transcripts.R
@@ -10,14 +10,3 @@ test_that("check that standard model is generated correctly", {
     expect_equal(length(select_transcripts("standard")), length(transcripts$standard))
 })
 
-test_that("check that TOP1500 (caps) is generated correctly", {
-    expect_equal(length(select_transcripts("TOP1500")), length(transcripts$tx1500))
-})
-
-test_that("check that cell component with all caps is generated correctly", {
-    expect_equal(length(select_transcripts("CELL_COMPONENT")), length(transcripts$cell_component))
-})
-
-test_that("check that STANDARD (caps) model is generated correctly", {
-    expect_equal(length(select_transcripts("STANDARD")), length(transcripts$standard))
-})
