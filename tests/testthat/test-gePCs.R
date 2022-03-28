e56c1a285b93e066227335a72a4accbc93a15c32
joshstolz <josh.stolz@libd.org>
2022-03-28 08:05:39 -0400

    add test that confirms assayname specified is present in the rse

--- tests/testthat/test-gePCs.R
+++ tests/testthat/test-gePCs.R
@@ -2,3 +2,8 @@
 test_that("output is a prcomp", {
     expect_equal(class(getPCs(covComb_tx_deg, "tpm")), "prcomp")
 })
+
+test_that("check for assayname break", {
+    expect_error(getPCs(covComb_tx_deg, "NA"))
+})
+
