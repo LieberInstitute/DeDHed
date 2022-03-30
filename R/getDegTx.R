3bcd974adb82bea98f41d498e2cf25d729327b00
joshstolz <josh.stolz@libd.org>
2022-03-30 16:02:25 -0400

    fix type parameter

--- R/getDegTx.R
+++ R/getDegTx.R
@@ -6,17 +6,18 @@
 #' necessary to remove the effect of degradation in differential expression.
 #'
 #' @param rse_tx A [RangedSummarizedExperiment-class][SummarizedExperiment::RangedSummarizedExperiment-class]
-#' object containing the transcript data desired to be studied. @param type A `character(1)` specifying the transcripts set type.
+#' object containing the transcript data desired to be studied.
+#' @param type A `character(1)` specifying the transcripts set type.
 #' These were determined by Joshua M. Stolz et al, 2022. Here the names "cell_component", "top1500",
 #' and "standard" refer to models that were determined to be effective in removing degradation effects.
 #' The "standard" model involves taking the union of the top 1000 transcripts
 #' associated with degradation from the interaction model and the main effect model.
-#'  The "top1500" model is the same as the "standard model except the
-#'  union of the top 1500 genes associated with degradation is selected.
-#'   The most effective of our models, "cell_component", involved deconvolution of
-#'   the degradation matrix to determine the proportion of cell types within our studied tissue.
-#'   These proportions were then added to our `model.matrix()` and the union of the top 1000 transcripts in the interaction model,
-#'    the main effect model, and the cell proportions model were used to generate this model of qSVs.
+#' The "top1500" model is the same as the "standard model except the
+#' union of the top 1500 genes associated with degradation is selected.
+#' The most effective of our models, "cell_component", involved deconvolution of
+#' the degradation matrix to determine the proportion of cell types within our studied tissue.
+#' These proportions were then added to our `model.matrix()` and the union of the top 1000 transcripts in the interaction model,
+#' the main effect model, and the cell proportions model were used to generate this model of qSVs.
 #'
 #' @param assayname character string specifying the name of the assay desired in rse_tx
 #' @param sig_transcripts A list of transcripts determined to have degradation signal in the qsva expanded paper.
