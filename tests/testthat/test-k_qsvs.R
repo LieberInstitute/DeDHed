4ef1276a95a19d9fb50ecc46b57ff1918b5577ed
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-06-21 17:16:18 -0400

    Resolve #28 by documenting where users need to specify set.seed() to ensure reproducibility of their results.
    Co-authored-by: Hedia Tnani <hediatnani0@gmail.com>

--- tests/testthat/test-k_qsvs.R
+++ tests/testthat/test-k_qsvs.R
@@ -8,24 +8,30 @@ mod2 <- mod
 mod2 <- cbind(mod2, 1 - mod[, "mitoRate"])
 colnames(mod2)[8] <- "mitotest"
 
+## Set a random seed
+set.seed(20230621)
+k_res <- k_qsvs(covComb_tx_deg, mod, "tpm")
+
 test_that("length pf output is 1", {
-    expect_equal(length(k_qsvs(covComb_tx_deg, mod, "tpm")), 1)
+    expect_equal(length(k_res), 1)
 })
 
 test_that("test that output for example data k = 10", {
-    expect_equal((k_qsvs(covComb_tx_deg, mod, "tpm")), 10)
+    expect_equal(k_res, 10)
 })
 
 test_that("output is an numeric", {
-    expect_equal(class(k_qsvs(covComb_tx_deg, mod, "tpm")), "numeric")
+    expect_equal(class(k_res), "numeric")
 })
 
 
 test_that("non-full rank data throws error", {
+    set.seed(20230621)
     expect_error(qSVA(rse_tx = covComb_tx_deg, type = "cell_component", mod = mod2, assayname = "tpm"))
 })
 
 
 test_that("test that full rank matrix produces check and error", {
+    set.seed(20230621)
     expect_error(k_qsvs(rse_tx_low, mod, "tpm"))
 })
--- tests/testthat/test-qSVA.R
+++ tests/testthat/test-qSVA.R
@@ -1,19 +1,24 @@
 mod <- model.matrix(~ mitoRate + Region + rRNA_rate + totalAssignedGene + RIN, data = colData(covComb_tx_deg))
 
+## Run qSVA with 2 types
+set.seed(20230621)
+qsva_cc <- qSVA(rse_tx = covComb_tx_deg, type = "cell_component", mod = mod, assayname = "tpm")
+set.seed(20230621)
+qsva_standard <- qSVA(rse_tx = covComb_tx_deg, type = "standard", mod = mod, assayname = "tpm")
 
 test_that("number of qsvs is k", {
-    expect_equal(length(colnames(qSVA(rse_tx = covComb_tx_deg, type = "cell_component", mod = mod, assayname = "tpm"))), 10)
-    expect_equal(length(colnames(qSVA(rse_tx = covComb_tx_deg, type = "standard", mod = mod, assayname = "tpm"))), 8)
+    expect_equal(length(colnames(qsva_cc)), 10)
+    expect_equal(length(colnames(qsva_standard)), 8)
 })
 
 test_that("length of qsv rownames is the same as number of samples", {
-    expect_equal(length(rownames(qSVA(rse_tx = covComb_tx_deg, type = "cell_component", mod = mod, assayname = "tpm"))), length(colnames(covComb_tx_deg)))
+    expect_equal(length(rownames(qsva_cc)), length(colnames(covComb_tx_deg)))
 })
 
 test_that("output is a matrix", {
-    expect_equal(class(qSVA(rse_tx = covComb_tx_deg, type = "cell_component", mod = mod, assayname = "tpm"))[1], "matrix")
+    expect_equal(class(qsva_cc)[1], "matrix")
 })
 
 test_that("output is an array", {
-    expect_equal(class(qSVA(rse_tx = covComb_tx_deg, type = "cell_component", mod = mod, assayname = "tpm"))[2], "array")
+    expect_equal(class(qsva_cc)[2], "array")
 })
