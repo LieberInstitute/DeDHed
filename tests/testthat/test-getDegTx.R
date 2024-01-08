f5a96821f0b06339f214d26378f728f35a9a1f60
HediaTnani <hediatnani0@gmail.com>
2024-01-08 09:48:16 -0500

    Add test to check getDegTx works with original and altered row names without the trailing .[0-9]

--- tests/testthat/test-getDegTx.R
+++ tests/testthat/test-getDegTx.R
@@ -30,6 +30,37 @@ test_that("getDegTx correctly processes covComb_tx_deg", {
   # Expect an error when rownames do not start with "ENST"
   expect_error(getDegTx(altered_covComb_tx_deg), "Error: Some rownames do not start with 'ENST'.")
 })
-  
 
+
+# Test where all sig_transcripts are in covComb_tx_deg rownames
+test_that("All sig_transcripts are in rse_tx rownames", {
+  sig_transcripts <- select_transcripts("cell_component")
+  expect_silent({
+    if (!any(sig_transcripts %in% rownames(covComb_tx_deg))) {
+      stop("sig_transcripts and rownames(rse_tx) do not match")
+    }
+  })
+})
+
+# Test where none of the sig_transcripts are in the rse_tx rownames
+test_that("No sig_transcripts are in rse_tx rownames", {
+  sig_transcripts <- c("gene4", "gene5", "gene6")
+  expect_error({
+    if (!any(sig_transcripts %in% rownames(covComb_tx_deg))) {
+      stop("sig_transcripts and rownames(rse_tx) do not match")
+    }
+  }, "sig_transcripts and rownames(rse_tx) do not match")
+})
+
+test_that("getDegTx works with original and altered row names", {
+  # Apply getDegTx to covComb_tx_deg
+  original_results <- getDegTx(covComb_tx_deg)
   
+  # Alter the row names of covComb_tx_deg and apply getDegTx
+  altered_covComb_tx_deg <- covComb_tx_deg
+  rownames(altered_covComb_tx_deg) <- gsub("\\..*", "", rownames(covComb_tx_deg))
+  altered_results <- getDegTx(altered_covComb_tx_deg)
+  
+  # Test if the results are equivalent
+  expect_equivalent(original_results, altered_results)
+})
