4df643fc4f11c9e56b81c0330ec31836a5e95df7
HediaTnani <hediatnani0@gmail.com>
2024-01-08 10:17:59 -0500

    Update comment

--- tests/testthat/test-getDegTx.R
+++ tests/testthat/test-getDegTx.R
@@ -61,6 +61,6 @@ test_that("getDegTx works with original and altered row names", {
   rownames(altered_covComb_tx_deg) <- gsub("\\..*", "", rownames(covComb_tx_deg))
   altered_results <- getDegTx(altered_covComb_tx_deg,select_transcripts("cell_component"))
   
-  # Test if the results are equivalent
+  # Test if the results are equal
   expect_equal(original_results, altered_results)
 })
