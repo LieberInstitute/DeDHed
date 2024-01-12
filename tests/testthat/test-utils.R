af15128d2ef603ea546e877ff21ace62a9c8b687
HediaTnani <hediatnani0@gmail.com>
2024-01-12 14:29:12 -0500

    Add unit test for mixed GENCODE and ENSEMBL annotation
    Co-authored-by: Nick Eagles <nickeagles77@gmail.com>

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -9,3 +9,17 @@ test_that("check_tx_names throw error if annotation is not GENCODE or ENSEMBL", 
   expect_error(check_tx_names(rownames(altered_covComb_tx_deg), sig_transcripts, 'rownames(rse_tx)', 'sig_transcripts'), "rownames\\(rse_tx\\)' must use either all GENCODE or all ENSEMBL transcript IDs")
 })
 
+# Test for check_tx_names transcripts throw errors if annotation is not mixed GENCODE and ENSEMBL
+test_that("Mixed row names annotations throw an error", {
+  # For testing the error condition, altered manually rownames of covComb_tx_deg
+  altered_covComb_tx_deg <- covComb_tx_deg
+  rownames(altered_covComb_tx_deg)[1] <- "ENST00000442987" # Change the first rowname 
+  rownames(altered_covComb_tx_deg)[2] <- "ENST00000623083" # Change the second rowname 
+  sig_transcripts <- select_transcripts("cell_component")
+  expect_error({
+    # Check if mixed row names annotations (ENST and ENST.*?\.) throw an error
+    check_tx_names(rownames(altered_covComb_tx_deg), sig_transcripts, 'rownames(rse_tx)', 'sig_transcripts')
+    
+  }, "rownames\\(rse_tx\\)' must use either all GENCODE or all ENSEMBL transcript IDs")
+})
+
