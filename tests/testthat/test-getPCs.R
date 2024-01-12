e65d1cf982f61e6b4e91f040395b802dd8fdf278
HediaTnani <hediatnani0@gmail.com>
2024-01-12 16:07:30 -0500

    Add getPcs to test_that instead of getDegTx

--- tests/testthat/test-getPCs.R
+++ tests/testthat/test-getPCs.R
@@ -7,6 +7,6 @@ test_that("check for assayname break", {
 })
 
 # Test for assayname not in assayNames
-test_that("getDegTx throws an error when assayname is not in assayNames", {
+test_that("getPCs throws an error when assayname is not in assayNames", {
   expect_error(getPCs(covComb_tx_deg, assayname = "not_in_assayNames"), "'not_in_assayNames' is not in assayNames\\(rse_tx\\).")
 })
