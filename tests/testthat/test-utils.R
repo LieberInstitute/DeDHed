f0193d481bec8fba90622ba38c18929f5d4e7af7
HediaTnani <hediatnani0@gmail.com>
2024-01-12 14:39:03 -0500

    Add a single quote

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,7 +7,7 @@ test_that("check_tx_names throw error if annotation is not GENCODE or ENSEMBL", 
   rownames(altered_covComb_tx_deg) <- paste0("gene", 1:length(rownames(altered_covComb_tx_deg))) 
   sig_transcripts <- select_transcripts("cell_component")
   expect_error(check_tx_names(rownames(altered_covComb_tx_deg), sig_transcripts, 'rownames(rse_tx)', 'sig_transcripts'), "rownames\\(rse_tx\\)' must use either all GENCODE or all ENSEMBL transcript IDs")
-  expect_error(check_tx_names(sig_transcripts, rownames(altered_covComb_tx_deg), 'sig_transcripts', 'rownames(rse_tx)'), "sig_transcripts' must use either all GENCODE or all ENSEMBL transcript IDs")
+  expect_error(check_tx_names(sig_transcripts, rownames(altered_covComb_tx_deg), 'sig_transcripts', 'rownames(rse_tx)'), "'sig_transcripts' must use either all GENCODE or all ENSEMBL transcript IDs")
 })
 
 # Test for check_tx_names transcripts throw errors if annotation is not mixed GENCODE and ENSEMBL
