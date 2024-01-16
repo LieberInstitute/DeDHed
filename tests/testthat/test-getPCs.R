60b878c0cb64a9cbae3cddb33a7ddb21f8dcdd00
HediaTnani <hediatnani0@gmail.com>
2024-01-16 16:11:30 -0500

    remove duplicated unit test for assayname
    Co-authored-by: Nick Eagles <nickeagles77@gmail.com>

--- tests/testthat/test-getPCs.R
+++ tests/testthat/test-getPCs.R
@@ -2,10 +2,6 @@ test_that("output is a prcomp", {
     expect_equal(class(getPCs(covComb_tx_deg, "tpm")), "prcomp")
 })
 
-test_that("check for assayname break", {
-    expect_error(getPCs(covComb_tx_deg, "NA"))
-})
-
 # Test for assayname not in assayNames
 test_that("getPCs throws an error when assayname is not in assayNames", {
   expect_error(getPCs(covComb_tx_deg, assayname = "not_in_assayNames"), "'not_in_assayNames' is not in assayNames\\(rse_tx\\).")
