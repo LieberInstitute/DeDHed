3c7cc0510fb6d7e5e09973562fbaa6c16ba9c22a
Nick-Eagles <nick.eagles@libd.org>
2024-11-05 09:18:08 -0500

    Use up-to-date code to generate the list of degradation-associated transcripts. The previous set was old, disagreed with current results, and didn't have a record of where it came from in https://github.com/LieberInstitute/QSVA_2020/tree/master

--- data-raw/transcripts.R
+++ data-raw/transcripts.R
@@ -1,25 +1,42 @@
-### internal data from the cluster
-# setwd("/dcs04/lieber/lcolladotor/qSVA_LIBD3080/degradation_experiments/Joint/all/")
+library(tidyverse)
 
-### this script runs on jhpce
-library("here")
+#   The top degradation-associated transcripts using each model was already
+#   computed in the QSVA_2020 repo at JHPCE; see:
+#   https://github.com/LieberInstitute/QSVA_2020/blob/a03958532e653c73a5c075be0afcb13a8cf5eeac/code/03_explore_transcripts/00_explore_transcripts_qsva/04_Modeling_NoVoom.R
+jhpce_dir = "/dcs04/lieber/lcolladotor/qSVA_LIBD3080/degradation_experiments/Joint/all"
+transcripts_path = file.path(
+    jhpce_dir, "preprocessed-data", "03_explore_transcripts", "Sig_Txs.RData"
+)
 
-### standard model
-load(here("rdas", "qsvs", "covCombs_withdlpfc.rda"))
-covComb_tx_deg_nocc <- covComb_tx_deg
-covComb_tx_deg_nocc_down <- covComb_tx_deg_down
+#   Grab the top N significant transcripts from the main and interaction models,
+#   then take their union
+get_top_n = function(tx_main, tx_int, num_tx) {
+    sig_tx_main = tx_main |>
+        rownames_to_column('tx') |>
+        as_tibble() |>
+        arrange(adj.P.Val) |>
+        slice_head(n = num_tx) |>
+        pull(tx)
 
-## cell component model
-load(here("rdas", "qsvs", "covCombs_withdlpfc_cc.rda"))
-covComb_tx_deg_cc <- covComb_tx_deg
-covComb_tx_deg_cc_down <- covComb_tx_deg_down
+    sig_tx_int = tx_int |>
+        rownames_to_column('tx') |>
+        as_tibble() |>
+        arrange(adj.P.Val) |>
+        slice_head(n = num_tx) |>
+        pull(tx)
 
-## 1500 transcripts model
-load(here("rdas", "qsvs", "covCombs_withdlpfc_1500.rda"))
+    return(union(sig_tx_main, sig_tx_int))
+}
 
-transcripts <- list(tx1500 = rownames(covComb_tx_1500), standard = rownames(covComb_tx_deg_nocc), cell_component = rownames(covComb_tx_deg_cc))
+load(transcripts_path)
 
-
-# save(transcripts, file = here("rdas", "qsvs", "model_transcripts.Rda"))
+transcripts <- list(
+    standard = get_top_n(outTxMain, outTxInt, 1000),
+    tx1500 = get_top_n(outTxMain, outTxInt, 1500),
+    cell_component = union(
+        get_top_n(outTxMain, outTxInt, 1000),
+        get_top_n(outTxMainSc, outTxIntSc, 1000)
+    )
+)
 
 usethis::use_data(transcripts, overwrite = TRUE)
